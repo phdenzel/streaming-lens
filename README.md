a44b5eb779477f65bdda971afe49321aa64fd117
phdenzel <phdenzel@gmail.com>
2018-08-02 18:31:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# lensing.js
+
+A javascript app that provides the module to lens images.
+
+Include [lensing.js](https://rawgit.com/phdenzel/lensing.js/master/lensing.js) with
+``` html
+<script type="text/javascript" src="https://rawgit.com/phdenzel/lensing.js/master/lensing.js"></script>
+```
---
+++ assets/js/streamlens.js
@@ -0,0 +1,55 @@
+// Grabbing settings
+var ww = window.innerWidth;
+var wh = window.innerHeight;
+var video = document.getElementById('video');
+var dst = document.getElementById('dst');
+
+
+function camInitSuccess() {
+    console.log('CamEngine starting...');
+    CamEngine.start();
+}
+
+
+function lensTracker(evt) {
+    var mp = LensModule.trackMouse(evt);
+    LensModule.setCenter(mp.x, mp.y);
+}
+
+
+function lensloadStream(resolve) {
+    console.log(resolve);
+    LensModule.readImage(video);
+    document.addEventListener('mousemove', lensTracker, false);
+}
+
+
+function lensProcessor(imgData) {
+    LensModule.readImage(video);
+    LensModule.setImageDataDst(imgData);
+    LensModule.process();
+    return imgData;
+}
+
+
+CamEngine.init({
+    video: video,
+    captureWidth: ww,
+    captureHeight: wh,
+    procWidth: ww,
+    procHeight: wh,
+    outputCvs: dst,
+    captureIntervalTime: 100,
+    initSuccessCallback: camInitSuccess,
+    processor: lensProcessor,
+    tolerance: -15
+});
+
+
+LensModule.init({
+    imageCtx: dst,
+    imageWidth: ww,
+    imageHeight: wh,
+    radius: 300,
+    initSuccessCallback: lensloadStream
+});
